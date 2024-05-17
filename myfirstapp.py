b0a49ad2bacb009d53282815a5ef12384d7a9aed
shaunyap90 <shaunyap90@gmail.com>
2024-05-17 16:04:40 +0800

    Add files via upload

---
+++ myfirstapp.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Add a title
+st.title("My First Streamlit App")
+
+# Add some text
+st.write("Hello, World!")
+
+# Add an input widget
+name = st.text_input("Enter your name", "Type here...")
+if name:
+    st.write(f"Hello, {name}!")
+    
